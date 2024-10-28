b84484827a72dddf2b749c00c6474ed758da4bd0
razzaat <rajat1127.be21@chitkara.edu.in>
2024-10-28 23:28:30 +0530

    update data

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,138 +1,138 @@
 const eventsData = [
   {
     id: 1,
-    name: "Music Festival",
-    date: "30-10-2024",
-    location: "City Park",
+    name: "Charity Marathon",
+    date: "01-12-2024",
+    location: "Marine Drive, Mumbai",
     description:
-      "Experience a vibrant evening of live music with top local bands and international headliners. Enjoy food stalls, merchandise booths, and interactive art installations. Bring your friends and family for a night of fun under the stars. Don’t miss the chance to discover new artists and genres!",
-    image: "./music_festival.jpg",
+      "Run for a cause at the annual Charity Marathon. Whether you're an experienced runner or just starting out, join the community to raise funds for local charities. Enjoy live music, refreshments, and post-race celebrations. Collect sponsorships to support your chosen cause.",
+    image: "./marathon.jpg",
   },
   {
     id: 2,
-    name: "Interactive Art Exhibition",
-    date: "05-11-2024",
-    location: "Art Gallery",
+    name: "Sustainable Gardening Workshop",
+    date: "15-01-2025",
+    location: "Lalbagh Botanical Garden, Bangalore",
     description:
-      "Dive into the world of modern art at our interactive exhibition. Featuring contemporary works, live artist demonstrations, and hands-on art stations for all ages. Engage with both emerging and established artists in a creative space. Participate in workshops that allow you to express your creativity.",
-    image: "./artexo.jpg",
+      "Join our Sustainable Gardening Workshop and learn about eco-friendly gardening techniques. Participate in hands-on activities, get tips from expert gardeners, and take home your very own plant to nurture and grow. Discover the benefits of organic gardening for the environment.",
+    image: "./garden.jpg",
   },
   {
     id: 3,
     name: "International Food Festival",
     date: "10-11-2024",
-    location: "Downtown",
+    location: "Sector 17, Chandigarh",
     description:
-      "Savor the flavors of the world at the International Food Festival. Sample a diverse array of dishes from local chefs and food vendors, participate in cooking classes, and enjoy live entertainment and culinary contests. Don’t forget to vote for your favorite dish in our popular contest!",
+      "Savor the flavors of the world at the International Food Festival. Sample a diverse array of dishes from local chefs and food vendors, participate in cooking classes, and enjoy live entertainment and culinary contests. Don't forget to vote for your favorite dish in our popular contest!",
     image: "./foodfest.jpg",
   },
   {
     id: 4,
-    name: "Literary Book Fair",
-    date: "15-11-2024",
-    location: "Convention Center",
+    name: "Gourmet Wine & Cheese Tasting",
+    date: "30-12-2024",
+    location: "Sula Vineyards, Nashik",
     description:
-      "Join us at the Literary Book Fair for author meet-and-greets, panel discussions, and book signings. Discover new genres and titles across fiction and non-fiction, and participate in engaging workshops for aspiring writers. Attend special sessions on self-publishing and marketing your work.",
-    image: "./bookFair.jpg",
+      "Indulge in a delightful evening of wine and cheese pairings at our Gourmet Tasting event. Enjoy a guided tour of the vineyard, learn about the art of winemaking, and savor a variety of wines paired with artisanal cheeses. Engage with sommeliers and enhance your palate with expert tips.",
+    image: "./wine&cheese.jpg",
   },
   {
     id: 5,
     name: "Innovation Tech Conference",
     date: "20-11-2024",
-    location: "Tech Hub",
+    location: "HITEC City, Hyderabad",
     description:
       "Engage with industry leaders at the Innovation Tech Conference. Attend keynote speeches, network with professionals, and take part in cutting-edge workshops that showcase the latest in technology and entrepreneurship. Explore startup showcases and connect with potential investors.",
     image: "./innovation.jpg",
   },
   {
     id: 6,
-    name: "Global Film Festival",
-    date: "25-11-2024",
-    location: "City Theater",
+    name: "Mindfulness and Wellness Retreat",
+    date: "01-02-2025",
+    location: "Rishikesh, Uttarakhand",
     description:
-      "Experience a curated selection of indie films from around the globe at the Global Film Festival. Attend exclusive screenings, engage in Q&A sessions with filmmakers, and participate in workshops focused on independent cinema. Network with fellow film enthusiasts and explore filmmaking techniques.",
-    image: "./filmfest.jpg",
+      "Take a break from your routine at the Mindfulness and Wellness Retreat. Engage in yoga sessions, guided meditations, and workshops focused on mental well-being and self-care. Connect with like-minded individuals in a serene setting. Enjoy nature walks and mindfulness exercises to rejuvenate your spirit.",
+    image: "./mindfulness.jpg",
   },
   {
     id: 7,
-    name: "Charity Marathon",
-    date: "01-12-2024",
-    location: "City Streets",
+    name: "Interactive Art Exhibition",
+    date: "05-11-2024",
+    location: "Kala Ghoda, Mumbai",
     description:
-      "Run for a cause at the annual Charity Marathon. Whether you're an experienced runner or just starting out, join the community to raise funds for local charities. Enjoy live music, refreshments, and post-race celebrations. Collect sponsorships to support your chosen cause.",
-    image: "./marathon.jpg",
+      "Dive into the world of modern art at our interactive exhibition. Featuring contemporary works, live artist demonstrations, and hands-on art stations for all ages. Engage with both emerging and established artists in a creative space. Participate in workshops that allow you to express your creativity.",
+    image: "./artexo.jpg",
   },
   {
     id: 8,
-    name: "STEM Science Fair",
-    date: "05-12-2024",
-    location: "Science Center",
-    description:
-      "Engage with the wonders of science at the STEM Science Fair. Explore interactive exhibits, attend live demonstrations, and participate in workshops led by scientists and educators. A perfect event for families and curious minds. Join competitions to win exciting science kits!",
-    image: "./stemsci.jpg",
-  },
-  {
-    id: 9,
     name: "Winter Holiday Market",
     date: "15-12-2024",
-    location: "Market Square",
+    location: "Mall Road, Shimla",
     description:
       "Get into the holiday spirit at the Winter Holiday Market. Shop for unique gifts, enjoy festive treats, and participate in fun holiday activities for the whole family. Live music and entertainment will create a joyful atmosphere. Join our community tree lighting ceremony for a magical evening.",
     image: "./holidaymarket.jpg",
   },
   {
+    id: 9,
+    name: "Music Festival",
+    date: "30-10-2024",
+    location: "Salt Lake Stadium, Kolkata",
+    description:
+      "Experience a vibrant evening of live music with top local bands and international headliners. Enjoy food stalls, merchandise booths, and interactive art installations. Bring your friends and family for a night of fun under the stars. Don't miss the chance to discover new artists and genres!",
+    image: "./music_festival.jpg",
+  },
+  {
     id: 10,
-    name: "Energetic Dance Workshop",
-    date: "20-12-2024",
-    location: "Dance Studio",
-    description:
-      "Join us for an Energetic Dance Workshop that welcomes all skill levels. Learn new styles, improve your technique, and meet fellow dance enthusiasts in a lively, supportive environment. Bring your enthusiasm and get ready to move! Experience the joy of dance through themed sessions.",
-    image: "./dance.jpg",
-  },
-  {
-    id: 11,
-    name: "Gourmet Wine & Cheese Tasting",
-    date: "30-12-2024",
-    location: "Vineyard",
-    description:
-      "Indulge in a delightful evening of wine and cheese pairings at our Gourmet Tasting event. Enjoy a guided tour of the vineyard, learn about the art of winemaking, and savor a variety of wines paired with artisanal cheeses. Engage with sommeliers and enhance your palate with expert tips.",
-    image: "./wine&cheese.jpg",
-  },
-  {
-    id: 12,
-    name: "Annual Photography Contest",
-    date: "10-01-2025",
-    location: "Photo Center",
-    description:
-      "Showcase your photography skills in our Annual Photography Contest. Submit your best work, receive constructive feedback from professionals, and compete for exciting prizes across multiple categories. Open to photographers of all levels. Attend workshops to improve your skills and gain insights from experts.",
-    image: "./photography.jpg",
-  },
-  {
-    id: 13,
-    name: "Sustainable Gardening Workshop",
-    date: "15-01-2025",
-    location: "Community Garden",
-    description:
-      "Join our Sustainable Gardening Workshop and learn about eco-friendly gardening techniques. Participate in hands-on activities, get tips from expert gardeners, and take home your very own plant to nurture and grow. Discover the benefits of organic gardening for the environment.",
-    image: "./garden.jpg",
-  },
-  {
-    id: 14,
     name: "Cultural Celebration Fest",
     date: "20-01-2025",
-    location: "City Square",
+    location: "City Palace, Jaipur",
     description:
       "Celebrate diversity at the Cultural Celebration Fest. Enjoy a variety of performances, food stalls, and craft workshops from different cultures. Participate in interactive activities that promote understanding and appreciation of global traditions. Engage with storytellers and artists to learn about their heritage.",
     image: "./culture.jpg",
   },
   {
-    id: 15,
-    name: "Mindfulness and Wellness Retreat",
-    date: "01-02-2025",
-    location: "Wellness Center",
+    id: 11,
+    name: "Global Film Festival",
+    date: "25-11-2024",
+    location: "PVR Director's Cut, Delhi",
     description:
-      "Take a break from your routine at the Mindfulness and Wellness Retreat. Engage in yoga sessions, guided meditations, and workshops focused on mental well-being and self-care. Connect with like-minded individuals in a serene setting. Enjoy nature walks and mindfulness exercises to rejuvenate your spirit.",
-    image: "./mindfulness.jpg",
+      "Experience a curated selection of indie films from around the globe at the Global Film Festival. Attend exclusive screenings, engage in Q&A sessions with filmmakers, and participate in workshops focused on independent cinema. Network with fellow film enthusiasts and explore filmmaking techniques.",
+    image: "./filmfest.jpg",
+  },
+  {
+    id: 12,
+    name: "Literary Book Fair",
+    date: "15-11-2024",
+    location: "Diggi Palace, Jaipur",
+    description:
+      "Join us at the Literary Book Fair for author meet-and-greets, panel discussions, and book signings. Discover new genres and titles across fiction and non-fiction, and participate in engaging workshops for aspiring writers. Attend special sessions on self-publishing and marketing your work.",
+    image: "./bookFair.jpg",
+  },
+  {
+    id: 13,
+    name: "STEM Science Fair",
+    date: "05-12-2024",
+    location: "Science City, Ahmedabad",
+    description:
+      "Engage with the wonders of science at the STEM Science Fair. Explore interactive exhibits, attend live demonstrations, and participate in workshops led by scientists and educators. A perfect event for families and curious minds. Join competitions to win exciting science kits!",
+    image: "./stemsci.jpg",
+  },
+  {
+    id: 14,
+    name: "Annual Photography Contest",
+    date: "10-01-2025",
+    location: "Victoria Memorial, Kolkata",
+    description:
+      "Showcase your photography skills in our Annual Photography Contest. Submit your best work, receive constructive feedback from professionals, and compete for exciting prizes across multiple categories. Open to photographers of all levels. Attend workshops to improve your skills and gain insights from experts.",
+    image: "./photography.jpg",
+  },
+  {
+    id: 15,
+    name: "Energetic Dance Workshop",
+    date: "20-12-2024",
+    location: "Attakkalari Centre, Bangalore",
+    description:
+      "Join us for an Energetic Dance Workshop that welcomes all skill levels. Learn new styles, improve your technique, and meet fellow dance enthusiasts in a lively, supportive environment. Bring your enthusiasm and get ready to move! Experience the joy of dance through themed sessions.",
+    image: "./dance.jpg",
   },
 ];
 
